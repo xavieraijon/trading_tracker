6723284e03859b83f1375bde3e2404426c4cdde7
Xavi Aijon <xavier.aijon@col.vueling.com>
2026-02-01 20:41:03 +0100

    build: complete project initialization with nestjs, angular, primeng, prisma, docker

---
+++ apps/web/src/app/app.config.ts
@@ -0,0 +1,24 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
+  ],
+};
