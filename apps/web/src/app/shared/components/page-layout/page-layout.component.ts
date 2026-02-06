26896e2ef36980f8821eb17d13aa4e1a284ea7d9
Xavi Aijon <xavier.aijon@col.vueling.com>
2026-02-06 19:08:41 +0100

    feat(shared): implement PageLayout component

---
+++ apps/web/src/app/shared/components/page-layout/page-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-layout.component.html',
+  styleUrl: './page-layout.component.scss'
+})
+export class PageLayoutComponent {
+  @Input({ required: true }) title: string = '';
+  @Input() subtitle?: string;
+}
